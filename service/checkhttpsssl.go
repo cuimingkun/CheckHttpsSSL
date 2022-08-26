ec2280d8f5a165f8078269a7d881953548914251
cuimingkun <cuimingkun@zongheng.com>
2022-08-26 18:59:30 +0800

    对30x跳转的域名进行兼容,只检测跳转前域名证书

--- service/checkhttpsssl.go
+++ service/checkhttpsssl.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"net/http"
 	"sync"
@@ -25,19 +26,32 @@ func CheckSSL(url []string) {
 	wg.Wait()
 }
 
-//检测单个域名的函数
+// 检测单个域名的函数
 func checksslOne(url string) {
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
-	client := &http.Client{Transport: tr}
+	// peerCertificates,声明此变量用于接收CheckRedirect的返回
+	var peerCertificates []*x509.Certificate
+	client := &http.Client{
+		Transport: tr,
+		// CheckRedirect针对30x跳转的处理函数,非跳转的不会执行此函数
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			// fmt.Printf("CheckRedirect req.Response.TLS.PeerCertificates: %#v\n", req.Response.TLS.PeerCertificates)
+			peerCertificates = req.Response.TLS.PeerCertificates
+			return nil
+		}}
 	resp, err := client.Get(url)
 	if err != nil {
 		fmt.Println(url, " 请求失败")
 		//panic(err)
 	} else {
 		defer resp.Body.Close()
-		certInfo := resp.TLS.PeerCertificates[0]
+		// 如果peerCertificates为空说明为非30x的域名,直接取TLS.PeerCertificates
+		if len(peerCertificates) == 0 {
+			peerCertificates = resp.TLS.PeerCertificates
+		}
+		certInfo := peerCertificates[0]
 		fmt.Println("==========================================")
 		fmt.Println("url:", url)
 		fmt.Println("过期时间:", certInfo.NotAfter)
